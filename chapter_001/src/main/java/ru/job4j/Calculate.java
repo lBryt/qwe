ee49985d17572d91a90f090fbfbcb00c9da4db04
IBryt <ihorbryt@gmail.com>
2018-05-01 16:01:33 +0300

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,13 @@
 package ru.job4j;
 
+/**
+* Package for calculate task.
+*
+* @author Ihor Bryt(ihorbryt@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+
 public class Calculate {
 	public static void main(String[] args ){
 		System.out.println("Hello World!");
