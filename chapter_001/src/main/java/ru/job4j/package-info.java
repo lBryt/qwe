b36fa92e406fca6f5cdbd7833b80fd3c8fa0bac7
IBryt <ihorbryt@gmail.com>
2018-05-02 23:19:14 +0300

    add plugin "maven-compiler-plugin" to parent pom.xml and add packege-info.

---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * //TODO add comments.
+ *
+ * @author Ihor Bryt (ihorbryt@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+package ru.job4j;
---
+++ chapter_001/src/test/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * //TODO add comments.
+ *
+ * @author Ihor Bryt (ihorbryt@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j;
