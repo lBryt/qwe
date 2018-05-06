975cf42c4c4e7b86a558f23157bf331b9f8b8388
IBryt <ihorbryt@gmail.com>
2018-05-06 19:52:35 +0300

    3.4. Максимум из трех чисел. Исправлена ошибка при валидации проекта

--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -16,7 +16,7 @@ public class MaxTest {
     @Test
     public void whenFirstMoreSecondAndThird() {
         Max maxim = new Max();
-        int result = maxim.max(3, 2,1);
+        int result = maxim.max(3, 2, 1);
         assertThat(result, Is.is(3));
     }
 }
