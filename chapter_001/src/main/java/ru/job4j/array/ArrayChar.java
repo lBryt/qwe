6ede7a9dd055f483e2689d86eebd0ccc25ffa693
IBryt <ihorbryt@gmail.com>
2018-05-11 21:35:22 +0300

    5.5. Слова начинается с ...

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -12,20 +12,17 @@ public class ArrayChar {
 
     /**
      * Проверяет. что слово начинается с префикса.
+     *
      * @param prefix префикс.
      * @return если слово начинаеться с префикса
      */
     public boolean startWith(String prefix) {
         boolean result = true;
         char[] value = prefix.toCharArray();
-        if (value.length > this.data.length) {
-            result = false;
-        } else {
-            for (int i = 0; i != value.length; i++) {
-                if (value[i] != this.data[i]) {
-                    result = false;
-                    break;
-                }
+        for (int i = 0; i != value.length; i++) {
+            if (value[i] != this.data[i]) {
+                result = false;
+                break;
             }
         }
         return result;
