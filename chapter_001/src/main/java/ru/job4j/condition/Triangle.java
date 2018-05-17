ff9180661740b85d128b6cbe018916b57b2d3891
IBryt <ihorbryt@gmail.com>
2018-05-17 04:37:54 +0300

    Исправление функции exist

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -55,6 +55,6 @@ public class Triangle {
      * @return Возвращает полупериметра;
      */
     private boolean exist(double ab, double ac, double bc) {
-        return (ab + ac > bc || ab + bc > ac || ac + bc > ab);
+        return ab + ac > bc && ab + bc > ac && ac + bc > ab;
     }
 }
