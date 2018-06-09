68c01e1b8b93554ba98f4048dbdbe9c060bdfb74
IBryt <ihorbryt@gmail.com>
2018-06-09 19:11:06 +0300

    1. Организовать сортировку User.

--- chapter_003/src/test/java/ru/job4j/sort/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/sort/SortUserTest.java
@@ -1,27 +1,31 @@
 package ru.job4j.sort;
 
-import org.junit.Before;
 import org.junit.Test;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class SortUserTest {
     private List<User> users = new ArrayList<>();
     private SortUser sort = new SortUser();
 
-    @Before
-    public void start() {
-        users.add(new User("Сергей", 25));
-//        users.add(new User("Иван", 30));
-//        users.add(new User("Сергей", 20));
-        users.add(new User("Иван", 25));
-    }
-
     @Test
     public void whenUserListSortByAgeThemGetListSortedInAscendingOrder() {
-        System.out.println(sort.sort(users));
+        User user1 = new User("Сергей", 25);
+        User user2 = new User("Иван", 30);
+        User user3 = new User("Сергей", 20);
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
+        Set<User> result = sort.sort(this.users);
+        Set<User> expected = new TreeSet<>();
+        expected.add(user3);
+        expected.add(user1);
+        expected.add(user2);
+        assertThat(expected, is(result));
     }
 }
